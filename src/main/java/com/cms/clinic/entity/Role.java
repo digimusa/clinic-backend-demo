bd361d3672af9a252bdf85a5c09c787c3f378c69
Mogau96 <Mogau@digilink.africa>
2023-06-22 09:11:24 +0200

    added patient class, role class, patient and role repositories

---
+++ src/main/java/com/cms/clinic/entity/Role.java
@@ -0,0 +1,17 @@
+package com.cms.clinic.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String roleName;
+}
--- src/main/java/com/cms/clinic/entity/User.java
+++ src/main/java/com/cms/clinic/entity/User.java
@@ -1,4 +1,38 @@
 package com.cms.clinic.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String contactNo;
+    private String email;
+    private String password;
+    private String address;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLE",
+            joinColumns = {
+                    @JoinColumn(name = "USER_ID")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID")
+            }
+    )
+    private Set<Role> roles;
+
+    private boolean isEnabled;
+
 }
