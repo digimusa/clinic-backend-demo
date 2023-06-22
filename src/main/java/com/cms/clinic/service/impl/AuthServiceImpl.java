30aff8f843d635773ded36aade0e877fd5b3b0a8
Mogau96 <Mogau@digilink.africa>
2023-06-22 15:17:20 +0200

    patient can register new account and login, all the users can login according to their roles

--- src/main/java/com/cms/clinic/service/impl/AuthServiceImpl.java
+++ src/main/java/com/cms/clinic/service/impl/AuthServiceImpl.java
@@ -1,13 +1,24 @@
 package com.cms.clinic.service.impl;
 
-import com.cms.clinic.dto.RegisterRequestDto;
+import com.cms.clinic.dto.AuthenticationRequestDto;
+import com.cms.clinic.dto.AuthenticationResponse;
 import com.cms.clinic.entity.Patient;
-import com.cms.clinic.entity.Role;
-import com.cms.clinic.exception.EmailAlreadyTakenException;
+import com.cms.clinic.entity.User;
+import com.cms.clinic.exception.AccountNotActivatedException;
+import com.cms.clinic.exception.InvalidCredentialsException;
+import com.cms.clinic.exception.UserDoesNotExistException;
+import com.cms.clinic.jwt.JwtRequestFilter;
 import com.cms.clinic.repositories.PatientRepository;
+import com.cms.clinic.repositories.UserRepository;
 import com.cms.clinic.service.AuthService;
+import com.cms.clinic.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,26 +27,40 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AuthServiceImpl implements AuthService {
 
-    private final PatientRepository patientRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtil jwtUtil;
 
     @Override
-    public Patient register(RegisterRequestDto registerRequest) {
-        log.info("Inside Register new patient method {} ", registerRequest);
-
-        Patient patient = new Patient();
-
-        patient.setFirstName(registerRequest.getFirstName());
-        patient.setLastName(registerRequest.getLastName());
-        patient.setEmail(registerRequest.getEmail());
-        patient.setRole(Role.PATIENT);
-        patient.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
-
+    public AuthenticationResponse login(AuthenticationRequestDto authenticationRequestDto) {
+        String email = authenticationRequestDto.getEmail();
+        String password = authenticationRequestDto.getPassword();
 
         try {
-            return patientRepository.save(patient);
-        } catch (Exception e){
-            throw new EmailAlreadyTakenException();
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(email, password)
+            );
+            if (auth.isAuthenticated()) {
+                if (userDetailsService.getUserDetail().getEnabled().equalsIgnoreCase("true")) {
+                    User patient = getUserByEmail(email);
+                    String jwtToken = jwtUtil.generateToken(email,patient.getRole().name());
+                    return new AuthenticationResponse(patient, jwtToken);
+                } else {
+                    throw new AccountNotActivatedException();
+                }
+            }
+        } catch (DisabledException e) {
+            e.printStackTrace();
+        } catch (BadCredentialsException e) {
+            throw new InvalidCredentialsException();
         }
+
+        return new AuthenticationResponse(null, "Bad credentials");
+    }
+
+    private User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserDoesNotExistException());
     }
 }
